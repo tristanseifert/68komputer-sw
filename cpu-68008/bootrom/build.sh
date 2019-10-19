7cf265b6acb4165f2b92fff6572b9154fd0dbd9c
Tristan Seifert <tristan@tseifert.me>
2019-10-19 18:02:53 -0500

    cleaned up vubug code some more

--- cpu-68008/bootrom/build.sh
+++ cpu-68008/bootrom/build.sh
@@ -6,10 +6,18 @@ date > builddate.txt
 
 # build loader
 echo "Assembling loader..."
-vasmm68k_mot -Fbin -m68008 -spaces -o bootrom.bin -L bootrom.lst bootrom.68k
+vasmm68k_mot -Fbin -m68008 -esc -spaces -o bootrom.bin -L bootrom.lst bootrom.68k
 
-# combine them into a rom image
+# build VUBUG
+echo "\n\nAssembling VUBUG..."
+vasmm68k_mot -Fbin -m68008 -esc -spaces -o vubug/vubug.bin -L vubug/vubug.lst vubug/VUBUG.68k
+
+# create some ROM images
 echo "\n\nCreating ROM image"
-rm -f rom.bin
+rm -f rom.bin rom_vubug.bin
+
 dd if=/dev/zero bs=131072 count=1 | LANG=C tr "\000" "\377" > rom.bin
+cp rom.bin rom_vubug.bin
+
 dd if=bootrom.bin of=rom.bin seek=0 conv=notrunc
+dd if=vubug/vubug.bin of=rom_vubug.bin seek=0 conv=notrunc
