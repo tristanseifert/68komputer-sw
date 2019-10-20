3a4879afb6ba88299cba44a674816b629a1ed5a3
Tristan Seifert <tristan@tseifert.me>
2019-10-19 20:10:34 -0500

    added enhanced basic code

--- cpu-68008/bootrom/build.sh
+++ cpu-68008/bootrom/build.sh
@@ -12,6 +12,11 @@ vasmm68k_mot -Fbin -m68008 -esc -spaces -o bootrom.bin -L bootrom.lst bootrom.68
 echo "\n\nAssembling VUBUG..."
 vasmm68k_mot -Fbin -m68008 -esc -spaces -o vubug/vubug.bin -L vubug/vubug.lst vubug/VUBUG.68k
 
+# build enhanced basic (delete everything before $8000 in output)
+echo "\n\nAssembling Enhanced BASIC…"
+vasmm68k_mot -Fbin -m68008 -esc -spaces -o ebasic/ebasic.bin -L ebasic/ebasic.lst ebasic/basic.68k
+dd if=ebasic/ebasic.bin of=ebasic/ebasic_trimmed.bin skip=32768 bs=1 conv=notrunc
+
 # create some ROM images
 echo "\n\nCreating ROM image"
 rm -f rom.bin rom_vubug.bin
@@ -21,3 +26,7 @@ cp rom.bin rom_vubug.bin
 
 dd if=bootrom.bin of=rom.bin seek=0 conv=notrunc
 dd if=vubug/vubug.bin of=rom_vubug.bin seek=0 conv=notrunc
+
+# into both ROMs, insert Enhanced BASIC at $8000
+dd if=ebasic/ebasic_trimmed.bin of=rom.bin seek=32768 bs=1 conv=notrunc
+dd if=ebasic/ebasic_trimmed.bin of=rom_vubug.bin seek=32768 bs=1 conv=notrunc
---
+++ cpu-68008/bootrom/ebasic/README.md
@@ -0,0 +1,3 @@
+This directory contains a version of Lee Davison's Enhanced Basic for 68000 modified to work with the 68komputer.
+
+Sources were copied from [Jeff Tranter's Github repository](https://github.com/jefftranter/68000/tree/master/ehbasic).
