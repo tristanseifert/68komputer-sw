fc847adce81933ccee80c79a2f700a87a7374b0c
Tristan Seifert <tristan@tseifert.me>
2019-11-01 11:55:54 -0500

    added code upload script

---
+++ cpu-68008/bootrom/tools/load.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+################################################################################
+# Loads a binary via serial.
+################################################################################
+import progressbar
+import serial
+from termcolor import colored, cprint
+import sys
+import os
+
+################################################################################
+# waits for an acknowledge
+def waitForAck(port):
+    # try to read a char
+    ret = port.read(1)
+
+    # handle not receiving anything
+    if len(ret) is not 1:
+        cprint('Failed to receive acknowledge!', 'red')
+        sys.exit(-1)
+    # ensure it was a dot for ack
+    elif ret[0] is not 0x2e:
+        cprint('Received 0x{char:02x} but expected "." as ack'.format(char=ret[0]))
+        sys.exit(-1)
+
+    # we good
+
+################################################################################
+# check arguments
+if len(sys.argv) is not 5:
+    print('usage: {exec} [binary path] [load address] [serial port] [baud rate]'.format(exec=sys.argv[0]))
+    sys.exit(-1)
+
+# open and configure the serial port with timeout of 1sec
+port = serial.Serial(sys.argv[3], int(sys.argv[4]), timeout=1)
+
+# print the load address and wait for an ack
+cprint('Sending load address', attrs=['bold'])
+port.write('{load:08x}'.format(load=int(sys.argv[2], 0)).encode('ascii'))
+waitForAck(port)
+
+# get the file size and print that
+size = os.path.getsize(sys.argv[1])
+
+cprint('Sending file size', attrs=['bold'])
+port.write('{size:08x}'.format(size=size).encode('ascii'))
+waitForAck(port)
+
+# then, send each byte of the file
+with progressbar.ProgressBar(max_value=size) as bar:
+    with open(sys.argv[1], "rb") as f:
+        byte = f.read(1)
+        counter = 0
+
+        while byte:
+            # send it and demand an ack
+            port.write('{data:02x}'.format(data=ord(byte)).encode('ascii'))
+            waitForAck(port)
+
+            # read the next byte
+            byte = f.read(1)
+
+            # update progress
+            counter += 1
+            bar.update(counter)
