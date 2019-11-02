88974a07bfb5c68f749f0a500a0d5b9757a883a2
Tristan Seifert <tristan@tseifert.me>
2019-11-02 17:12:24 -0500

    added more loader services; some rtc tests

--- cpu-68008/bootrom/user/build.sh
+++ cpu-68008/bootrom/user/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo "Assembling 68681 IO test"
 vasmm68k_mot -Fbin -m68008 -esc -spaces -o 68681_io.bin -L 68681_io.lst 68681_io.68k
+
+echo "Assembling DS1244 Test"
+vasmm68k_mot -Fbin -m68008 -esc -spaces -o ds1244_test.bin -L ds1244_test.lst ds1244_test.68k
+vasmm68k_mot -Fbin -m68008 -esc -spaces -o ds1244_set.bin -L ds1244_set.lst ds1244_set.68k
