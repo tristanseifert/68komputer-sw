5c1a26b2c3173a00f432ec0a1af8093517171497
Tristan Seifert <tristan@tseifert.me>
2019-11-07 18:47:56 -0600

    Better heap/BSS initialization Compacted some ANSI escape sequences in boot ROM

--- cpu-68008/c/stdlib/platform/init.c
+++ cpu-68008/c/stdlib/platform/init.c
@@ -1,38 +1,52 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include <tinyalloc.h>
-
 extern uint32_t __heap_start;
 extern uint32_t __heap_end;
 
 extern uint32_t __bss_start;
-extern uint32_t __end;
+extern uint32_t __bss_end;
+
+// helpers to clear stuff
+static void clear_bss();
+static void clear_heap();
+
 
 /**
  * C runtime initialization (before main)
  */
 void __c_init() {
-  // zero the bss
-  uint32_t *bss = (uint32_t *) &__bss_start;
-
-  do {
-    *bss = 0;
-  } while((uint32_t) bss++ <= (uint32_t) &__end);
-
-  // zero the heap
-  // uint8_t *heap = (uint8_t *) &__heap_start;
-  //
-  // do {
-  //   *heap = 0x00;
-  // } while((uint32_t) heap++ <= (uint32_t) &__heap_end);
-
-  // initialize the heap
-  ta_init(&__heap_start, &__heap_end, 256, 16, 8);
+  // clear various memory regions
+  clear_bss();
+  clear_heap();
 
   // TODO: call initializers
+}
 
-  // printf("Heap from $%p to $%p\r\n", &__heap_start, &__heap_end);
+/**
+ * Clears the BSS.
+ */
+static void clear_bss() {
+  register uint32_t *bss = (uint32_t *) &__bss_start;
+  register unsigned int size = (&__bss_end-&__bss_start);
+
+  // clear it in blocks of 4
+  for(int i = 0; i < (size / 4); i++) {
+    *bss++ = 0;
+  }
+}
+
+/**
+ * Clears the heap.
+ */
+static void clear_heap() {
+  register uint32_t *heap = (uint32_t *) &__heap_start;
+  register unsigned int size = (&__heap_end-&__heap_start);
+
+  // clear it in blocks of 4
+  for(int i = 0; i < (size / 4); i++) {
+    *heap++ = 0;
+  }
 }
 
 /**
--- cpu-68008/c/stdlib/platform/malloc.c
+++ cpu-68008/c/stdlib/platform/malloc.c
@@ -3,31 +3,34 @@
  */
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdbool.h>
 
-#include <tinyalloc.h>
+#include "umm_malloc.h"
 
 /**
  * Allocates a memory block and returns its address, or NULL if failed.
  */
 void *malloc(size_t size) {
-  return ta_alloc(size);
+  return umm_malloc(size);
 }
 
 /**
  * Allocates multiple elements of the same size
  */
 void *calloc(size_t count, size_t size) {
-  return ta_calloc(count, size);
+  return umm_calloc(count, size);
 }
 
 /**
  * Frees a memory block. In case this fails, we sit here forever.
  */
 void free(void *block) {
-  bool success = ta_free(block);
+  umm_free(block);
+}
 
-  if(!success) {
-    // TODO: log a message?
-    while(1) {}
-  }
+/**
+ * Re-allocates a block to a different size while preserving data.
+ */
+void *realloc(void *ptr, size_t size) {
+  return umm_realloc(ptr, size);
 }
